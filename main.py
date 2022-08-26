83e140edadf2f3e356705e794213b04e80f72925
prathamn1 <prathamnagaria@gmail.com>
2022-08-26 19:51:29 +0530

    changed window name

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Brawler")
+pygame.display.set_caption("Street Fighter")
 
 #set framerate
 clock = pygame.time.Clock()
